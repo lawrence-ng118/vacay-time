2fc336292b1c088c2ec31fe08db9f6241953fd07
lawrence-ng118 <lawrence.ng@student.ateneo.edu>
2024-06-29 15:02:40 +0800

    random shit ngl
    the real shit is in the notebook file HAHAHAHAHAH

---
+++ main.py
@@ -0,0 +1,22 @@
+originalcurrency = input("Please enter currency you will use : ")
+
+# ------------------------------------
+# webscrape based on amt of years selected for history
+
+exchangerates = {}
+
+print(type(exchangerates))
+
+# ------------------------------------
+
+num_lists = int(input("How many lists do you want? "))
+lists = []
+name = "webscraped currencies"
+addlist = "webscraped exchange rates from original"
+for p in range(num_lists):
+    lists.append(name)
+    lists.append([])
+
+def evaluation():
+
+    pass
